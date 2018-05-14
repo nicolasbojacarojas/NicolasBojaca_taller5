1f79d4db5beb337a19dd63b622cfe27a97767343
nicobojacarojas <nicobojacarojas@hotmail.com>
2018-05-14 00:46:09 -0500

    primera versiono

---
+++ punto_2/animacion.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+data = np.loadtxt('data.txt')
+fig, ax = plt.subplots()
+x = data[:, 1]
+y = data[:, 2]
+
+line, = ax.plot(x, y)	
+def animate(i):
+    line.set_ydata(y)  # update the data
+    return line,
+    
+def init():
+    line.set_ydata(np.ma.array(x, mask=True))
+    return line,
+
+plt.ylim(-1,1)
+ani = animation.FuncAnimation(fig, animate, init_func=init, interval=10 ,blit=True)
+ani.save('cuerda.gif', writer='imagemagick', fps=60)
+
